ddead20a0ba45bfa9976178469e1b5951d261f52
Tim Officer <timothyofficer@gmail.com>
2015-02-18 21:16:52 -0500

    cleanup

--- src/app/controllers/schedulePickerCtrl.js
+++ src/app/controllers/schedulePickerCtrl.js
@@ -8,14 +8,5 @@ angular.module('tablestrapApp').controller('schedulePickerCtrl', [
     $scope.logValues = function() {
       console.log($scope.form.schedule);
     };
-
-    $scope.$watch('form.schedule', function(val) {
-    	console.log(val);
-    });
-
-    $scope.submit = function(form) {
-
-      var foo = form;
-    };
   }
 ]);
